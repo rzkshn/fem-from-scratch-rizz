7a70b48e686a9be7cd296677529daf037518fc43
Rizka Shinta Wulandari <rizkashintaw@gmail.com>
2025-07-25 19:04:08 +0700

    finished

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ navToggle.addEventListener("click", () => {
   }
   console.log(navOpened);
 });
+
+const resizeObserver = new ResizeObserver(() => {
+  document.body.classList.add("resizing");
+
+  requestAnimationFrame(() => {
+    document.body.classList.remove("resizing");
+  });
+});
+
+resizeObserver.observe(document.body);
